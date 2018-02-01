cc6dfc8f35b6e157a183337671c9fc500973ef8b
adityak29 <adityakislay@yahoo.com>
2018-01-31 19:50:08 -0800

    https://github.com/adityak29/TestSam-Aditya.git

---
+++ DemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            // ViewData.Values.IsReadOnly.Equals()=new List<digit>();
+            return View();
+        }
+
+        [HttpPost]
+        public string DeIndex(string idigit)
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoMvc/Models/DigitClass.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoMvc.Models
+{
+    public class DigitClass
+    {
+        public int digit { get; set; }
+
+        public int idigit { get; set; }
+    }
+}
